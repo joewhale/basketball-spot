c33531fd58d1cbffab088dee162c5d653ccb8023
Joel Choi <joelc316@gmail.com>
2016-10-31 20:10:37 +0000

    add user to places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031194042) do
+ActiveRecord::Schema.define(version: 20161031200208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20161031194042) do
     t.string   "restroom"
     t.string   "water"
     t.string   "parking"
+    t.integer  "user_id"
   end
+
+  add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
